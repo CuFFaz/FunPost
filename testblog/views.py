de0608742628764c121e7c87eb80fa977e143f28
CuFFaz <drex8652@gmail.com>
2022-08-04 17:01:22 +0530

    Added view and template

--- testblog/views.py
+++ testblog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 from django.utils import timezone
 from django.contrib.auth.models import User
 from .models import Post
+from django.shortcuts import render, get_object_or_404
 
 
 def post_list(request):
     
-    me = User.objects.get(username='cuffaz')
+    me = User.objects.get(username='cuff')
 
     Post.objects.create(author=me, title="First_title", text="I adore wordplay", created_date=timezone.now()) 
     Post.objects.create(author=me, title="Second_title", text="I could adore wordplay", created_date=timezone.now()) 
@@ -18,3 +19,7 @@ def post_list(request):
     for p in post:
         p.publish()
     return render(request, "blog/post_list.html", {'posts':post})
+
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)    
+    return render(request, 'post_details.html', {'post':post})
