58b1a20d77b06a7edc0ca2a121ae1df8410f6743
CuFFaz <drex8652@gmail.com>
2022-08-04 16:11:43 +0530

    Added templates

--- testblog/views.py
+++ testblog/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.utils import timezone
+from django.contrib.auth.models import User
 from .models import Post
 
+
 def post_list(request):
+    
+    me = User.objects.get(username='cuff')
+
+    Post.objects.create(author=me, title="First_title", text="I adore wordplay", created_date=timezone.now()) 
+    Post.objects.create(author=me, title="Second_title", text="I could adore wordplay", created_date=timezone.now()) 
+    Post.objects.create(author=me, title="Third_title", text="I would adore wordplay", created_date=timezone.now()) 
+    Post.objects.create(author=me, title="Fourth_title", text="I play with wordplay", created_date=timezone.now()) 
+    Post.objects.create(author=me, title="Fifth_title", text="I may adore wordplay", created_date=timezone.now()) 
+
     post = Post.objects.all()
+    for p in post:
+        p.publish()
     return render(request, "blog/post_list.html", {'posts':post})
